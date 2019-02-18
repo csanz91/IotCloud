9ab0fdae810263eb2688032a4dce9d4325da2f44
csanz91 <cesarsanz.91@gmail.com>
2019-02-18 09:42:09 +0100

    Gateway: improve the values filtering by analizing the recorded values

--- python-gateway/source/max_list.py
+++ python-gateway/source/max_list.py
@@ -1,6 +1,8 @@
 import logging
 import logging.config
+import time
 from collections import defaultdict
+from cache_decorator import cache_disk
 
 logger = logging.getLogger()
 
@@ -29,7 +31,8 @@ class MaxSizeList(object):
             return None
         return sum(selfList)/float(lenSelfList)
 
-    def addValueSafe(self, newValue):
+    def addValueSafe(self, newValue, valueRange):
+        
         median = self.getMedian()
         # Anyway we add the value to the list, this way we get 
         # the chance to correct a false negative
@@ -38,9 +41,39 @@ class MaxSizeList(object):
         if not median:
             return
 
-        maxValueDelta = median * self.maxPercentageVariation
+        if valueRange:
+            maxValueDelta = abs(valueRange) * self.maxPercentageVariation
+        else:
+            maxValueDelta = abs(median) * self.maxPercentageVariation
+
         try:
             assert median-maxValueDelta < newValue < median+maxValueDelta
         except AssertionError:
             logger.error("The value is not valid. %s < %s < %s " %(median-maxValueDelta, newValue, median+maxValueDelta))
             raise ValueError("The value is outside the acceptable range")
+
+@cache_disk()
+def getValueRange(influxDb, locationId, sensorId):
+    oneDayAgo = int(time.time()) - 3600 * 24
+    query = ''' SELECT 
+                    MAX(value) - MIN(value) as range,
+                    COUNT(value) as numValues
+                FROM
+                    sensorsData 
+                WHERE
+                    locationId='%s' AND sensorId='%s' AND time>=%is
+            ''' % (locationId, sensorId, oneDayAgo)
+
+    results = influxDb.query(query)
+    if not results:
+        return
+
+    data = list(results.get_points())[0]
+    
+    numValues = data["numValues"]
+    if numValues<100:
+        logger.error("There is not enough values: %s to calculate the range" % numValues)
+        return None
+
+    valueRange = data["range"]
+    return valueRange
