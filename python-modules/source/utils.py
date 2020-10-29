1abeafd9a0bf6552ea7b728036130cf915d7e97e
Cesar Sanz <cesarsanz.91@gmail.com>
2020-10-29 18:51:14 +0100

    modules: formmating

--- python-modules/source/utils.py
+++ python-modules/source/utils.py
@@ -6,31 +6,39 @@ from dateutil import tz
 
 logger = logging.getLogger()
 
+
 def decodeBoolean(value):
     value = value.decode()
     assert value.lower() in ["true", "false"]
-    state = value.lower()=="true"
+    state = value.lower() == "true"
     return state
+
 
 def decodeStatus(value):
     value = value.decode()
     assert value.lower() in ["online", "offline"]
-    status = value.lower()=="online"
+    status = value.lower() == "online"
     return status
+
 
 def pushValue(mqttClient, tags, endpoint, value, retain=False):
     try:
-        topic = "v1/{locationId}/{deviceId}/{sensorId}/aux/{endpoint}".format(locationId=tags["locationId"],
-                                                                              deviceId=tags["deviceId"],
-                                                                              sensorId=tags["sensorId"],
-                                                                              endpoint=endpoint)
+        topic = "v1/{locationId}/{deviceId}/{sensorId}/aux/{endpoint}".format(
+            locationId=tags["locationId"],
+            deviceId=tags["deviceId"],
+            sensorId=tags["sensorId"],
+            endpoint=endpoint,
+        )
         mqttClient.publish(topic, value, retain=retain)
     except:
-        logger.error("Cant publish the value: %s in the endpoint: %s" % (value, endpoint))
+        logger.error(
+            "Cant publish the value: %s in the endpoint: %s" % (value, endpoint)
+        )
+
 
 def retryFunc(func):
     def wrapper(*args, **kwargs):
-    
+
         maxRetries = 10
         numRetries = 1
 
@@ -39,24 +47,30 @@ def retryFunc(func):
                 result = func(*args, **kwargs)
                 return result
             except:
-                logger.error('%s: Unable the complete the task. Retries %s/%s.' % (func.__name__, numRetries, maxRetries))
+                logger.error(
+                    "%s: Unable the complete the task. Retries %s/%s."
+                    % (func.__name__, numRetries, maxRetries)
+                )
 
             if numRetries >= maxRetries:
                 return result
-            time.sleep(numRetries**2+10)
+            time.sleep(numRetries ** 2 + 10)
             numRetries += 1
 
     return wrapper
+
 
 def getLocalTime(timeZoneId):
     localZone = tz.gettz(timeZoneId)
     now = datetime.datetime.now(tz=localZone)
     return now
 
+
 def getCurrentMinute(timeZoneId):
     now = getLocalTime(timeZoneId)
     currentMinute = now.hour * 60 + now.minute
     return currentMinute
+
 
 def getMinutesConverted(minutes, timeZoneId):
     localZone = tz.gettz(timeZoneId)
