d6233598fd1f7d63ad3407fd032841aea8410db7
Cesar Sanz <cesarsanz.91@gmail.com>
2020-10-30 22:29:12 +0100

    modules: extend sunchedule for 1 hour if the data received is already expired

--- python-modules/source/schedule.py
+++ python-modules/source/schedule.py
@@ -133,7 +133,7 @@ class Schedule:
 
             # If the data is already expired, renew it for an hour
             if now > self.utcSunScheduleExpireDate:
-                self.utcSunScheduleExpireDate += datetime.timedelta(hours=1)
+                self.utcSunScheduleExpireDate = now + datetime.timedelta(hours=1)
 
         self.runSunSchedule(mqttClient, today, currentMinute, timeZoneId)
         self.runManualSchedule(mqttClient, today, currentMinute, timeZoneId)
