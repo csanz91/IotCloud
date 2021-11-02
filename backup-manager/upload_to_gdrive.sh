2a52e59adc0c74bed571a73223b447d4bd842717
Cesar Sanz <cesarsanz.91@gmail.com>
2021-11-02 06:57:28 +0100

    Retrieve the secret file directly

--- backup-manager/upload_to_gdrive.sh
+++ backup-manager/upload_to_gdrive.sh
@@ -1,6 +1,5 @@
 #!/bin/ash
 echo 'Starting backups upload'
 
-cp /run/secrets/gdrive_config /gdrive/config/
 backup_folder=`cat /run/secrets/gdrive_backup_folder`
-gdrive -c /gdrive/config/ --service-account gdrive_config sync upload --delete-extraneous /backups/ $backup_folder
+gdrive -c / --service-account /run/secrets/gdrive_config sync upload --delete-extraneous /backups/ $backup_folder
